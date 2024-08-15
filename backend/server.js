b144f300fc0360df5b1cb6834bd825215ed19c44
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-15 15:08:24 +0530

    Configure the backend and frontend Using ejs in the backend Using vite in as a build tool in the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get("/",(req,res)=> {
-    res.send("hi")
+    const filePath = path.join(__dirname, '../frontend/amazon.html');
+    res.sendFile(filePath) //D:\Jeet\WebDev\amazon-react-clone\frontend\amazon.html
 })
 app.listen(3000)
