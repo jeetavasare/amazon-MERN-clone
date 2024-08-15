90532e3455117339f88e2c4a38333927d7424f0c
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-15 15:28:16 +0530

    Initialized React rendering in home page

---
+++ frontend/components/ProductsGrid.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductsContainer from "./ProductsContainer";
+const ProductsGrid = () =>{
+    return <>
+        <div className="products-grid">
+            <ProductsContainer/>
+        </div>
+    </>
+}
+
+export default ProductsGrid
