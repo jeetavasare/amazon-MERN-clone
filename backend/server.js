ef51898bc5e606f53012a9e5aef4d152bd8b2f00
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-16 22:16:41 +0530

    Connected react frontend with express backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const path = require('path');
+const cors = require("cors");
 const app = express()
 
-app.set("view engine", "ejs")
+
+app.use(cors({
+    origin: 'http://localhost:5173'  // Replace with your frontend origin
+}));
+
 
 app.get("/",(req,res)=> {
-    const filePath = path.join(__dirname, '../frontend/amazon.html');
-    res.sendFile(filePath) //D:\Jeet\WebDev\amazon-react-clone\frontend\amazon.html
+    res.send("Lol")
 })
+
+const router = require("./routes/products")
+
+app.use("/products",router)
+
+
 app.listen(3000)
--- frontend/components/ProductsContainer.jsx
+++ frontend/components/ProductsContainer.jsx
@@ -1,56 +1,74 @@
-import React from "react";
-
-const ProductsContainer = () =>{
-    return <>
-        <div className="product-container">
+import React, { useEffect, useState } from "react";
+const ProductsContainer = () => {
+  let [product,setProduct] = useState()
+  useEffect(
+    () =>{
+      const fetchProduct = async()=>{
+        let response = await fetch("http://localhost:3000/products")
+        let json = await response.json()
+        setProduct(json)
+      }
+      fetchProduct()
+    }
+    ,[])
+  if (!product){
+    return <h1>Loading</h1>
+  }
+  return (
+    <>
+      
+      <div className="product-container">
         <div className="product-image-container">
-            <img className="product-image"
-              src="images/products/athletic-cotton-socks-6-pairs.jpg"/>
-          </div>
-
-          <div className="product-name limit-text-to-2-lines">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
-          </div>
-
-          <div className="product-rating-container">
-            <img className="product-rating-stars"
-              src="images/ratings/rating-45.png"/>
-            <div className="product-rating-count link-primary">
-              87
-            </div>
-          </div>
-
-          <div className="product-price">
-            $10.90
-          </div>
-
-          <div className="product-quantity-container">
-            <select>
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div className="product-spacer"></div>
-
-          <div className="added-to-cart">
-            <img src="images/icons/checkmark.png"/>
-            Added
-          </div>
-
-          <button className="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
+          <img
+            className="product-image"
+            src={product[0]?.image}
+          />
         </div>
-    </>
-}
 
-export default ProductsContainer
+        <div className="product-name limit-text-to-2-lines">
+          {product[0]?.name}
+        </div>
+
+        <div className="product-rating-container">
+          <img
+            className="product-rating-stars"
+            src="images/ratings/rating-45.png"
+          />
+          <div className="product-rating-count link-primary">{product[0]?.rating?.count}</div>
+        </div>
+
+        <div className="product-price">{product[0]?.priceCents}</div>
+
+        <div className="product-quantity-container">
+          <select>
+            <option selected value="1">
+              1
+            </option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        <div className="product-spacer"></div>
+
+        <div className="added-to-cart">
+          <img src="images/icons/checkmark.png" />
+          Added
+        </div>
+
+        <button className="add-to-cart-button button-primary">
+          Add to Cart
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default ProductsContainer;
