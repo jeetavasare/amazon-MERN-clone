9dd69725d06e65e2245294d3cb4e55b4b3f03d02
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-22 18:18:31 +0530

    Now services can be registered as single instance or a shared instance via addSingleton() and addTransient() respectively Fixed typo instances'

--- backend/services.js
+++ backend/services.js
@@ -1,47 +1,60 @@
 class Services {
   constructor() {
     this.services = {};
-    this.instaces = {};
+    this.instances = {};
   }
 
   // Add a service with a name
-  add(name, service,args=[]) {
+  add(name, service, args = [], type="singleton") {
     if (typeof service !== 'function') {
       throw new Error('Service must be a function or class');
     }
-        this.services[name] = {service:service,serviceArgs:args,instantiated:false};
+    this.services[name] = { service: service, serviceArgs: args, instantiated: false, type: type };
   }
 
-  
+
+  addSingleton(name, service, args = []) {
+    this.add(name, service, args, "singleton")
+  }
+
+  addTransient(name, service, args = []) {
+    this.add(name, service, args, "transient")
+  }
+
 
   // Retrieve a service by name
   getService(name) {
     const serviceObj = this.services[name];
-    
+
     //check id serice was registered
     if (!serviceObj) {
       //throw error if service was not registered
       throw new Error(`Service ${name} was not registered`);
     }
 
-    //check if service is already instantiated
-    if (serviceObj.instantiated){
-      //return the stale service
-      return this.instaces[name]
+    if (serviceObj.type == "transient") {
+      //check if service is already instantiated
+      if (serviceObj.instantiated) {
+        //return the stale service
+        return this.instances[name]
+      }
+
+      //parse service args
+      const serviceArgs = serviceObj.serviceArgs
+
+      //create new instance of the service and store it
+      this.instances[name] = new serviceObj.service(...serviceArgs)
+      serviceObj.instantiated = true
+      //return the newly intantiated service
+      return this.instances[name];
     }
-    
-    //parse service args
-    const serviceArgs = serviceObj.serviceArgs
-    
-    //create new instance of the service and store it
-    this.instaces[name] = new serviceObj.service(...serviceArgs)
-    serviceObj.instantiated = true
-    //return the newly intantiated service
-    return this.instaces[name];
+    else if(serviceObj.type == "singleton"){
+      return this.getNewService(serviceObj)
+    }
+
   }
 
-  getNewService(name){
-    const serviceObj = this.services[name];
+  getNewService(serviceObj) {
 
     //check id serice was registered
     if (!serviceObj) {
