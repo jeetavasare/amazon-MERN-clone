2943bd89ff7f3c943cf75ab46c76f93614c43f25
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-22 17:01:54 +0530

    Now service registration container can return existing service if it was already created To get a new service anyways use getNewService() AppDB will give a new connection string only if not already connected in it's current intance, before eveytime connect() method was called it would return the new connection always

--- backend/db/AppDB.js
+++ backend/db/AppDB.js
@@ -4,20 +4,20 @@ import mongoose from "mongoose";
 class AppDB{
     constructor(connstr) {
         this.connstr = connstr
+        this.connection = null
     }
     async connect() {
+      if(this.connection){return this.connection}
         try {
           await mongoose.connect(this.connstr);
-          this.db = mongoose.connection;
+          this.connection = mongoose.connection;
           console.log("Connection Successful")
         }
         catch (err) {
           console.log("There was a problem connecting to the DataBase", err)
         }
     }
-    connection(){
-        return this.db
-    }
+    
     
 }
 
