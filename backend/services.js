c50e88d83575ee6ab8337b274f5ea74964583aea
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-22 17:01:54 +0530

    Now service registration container can return existing service if it was already created To get a new service anyways use getNewService() AppDB will give a new connection string only if not already connected in it's current intance, before eveytime connect() method was called it would return the new connection always

--- backend/services.js
+++ backend/services.js
@@ -1,26 +1,58 @@
 class Services {
   constructor() {
     this.services = {};
+    this.instaces = {};
   }
 
   // Add a service with a name
-  add(name, service,args) {
+  add(name, service,args=[]) {
     if (typeof service !== 'function') {
       throw new Error('Service must be a function or class');
     }
-        this.services[name] = {serviceName:service,serviceArgs:args};
+        this.services[name] = {service:service,serviceArgs:args,instantiated:false};
   }
 
   
 
   // Retrieve a service by name
   getService(name) {
-    const service = this.services[name].serviceName;
-    const serviceArgs = this.services[name].serviceArgs
-    if (!service) {
-      throw new Error(`Service ${name} not found`);
+    const serviceObj = this.services[name];
+    
+    //check id serice was registered
+    if (!serviceObj) {
+      //throw error if service was not registered
+      throw new Error(`Service ${name} was not registered`);
     }
-    return new service(...serviceArgs);
+
+    //check if service is already instantiated
+    if (serviceObj.instantiated){
+      //return the stale service
+      return this.instaces[name]
+    }
+    
+    //parse service args
+    const serviceArgs = serviceObj.serviceArgs
+    
+    //create new instance of the service and store it
+    this.instaces[name] = new serviceObj.service(...serviceArgs)
+    serviceObj.instantiated = true
+    //return the newly intantiated service
+    return this.instaces[name];
+  }
+
+  getNewService(name){
+    const serviceObj = this.services[name];
+
+    //check id serice was registered
+    if (!serviceObj) {
+      //throw error if service was not registered
+      throw new Error(`Service ${name} was not registered`);
+    }
+
+    //parse service args
+    const serviceArgs = serviceObj.serviceArgs
+    //return a new instance of the service
+    return new serviceObj.service(...serviceArgs)
   }
 }
 
