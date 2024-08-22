ec6d83c12ed9d02221cbe08453f44591cdbda818
jeetavasare <92804033+jeetavasare@users.noreply.github.com>
2024-08-22 20:41:54 +0530

    Now added nested dependencies support in the DI container To pass a service to another service use servicename in the args prefixed by $ e.g. `services.addSingleton("A",A) services.addTransient("B",B,["$A"]) ` Here B requires instance of A which will be created and injected into B at runtime

--- backend/services.js
+++ backend/services.js
@@ -5,7 +5,7 @@ class Services {
   }
 
   // Add a service with a name
-  add(name, service, args = [], type="singleton") {
+  add(name, service, args = [], type = "singleton") {
     if (typeof service !== 'function') {
       throw new Error('Service must be a function or class');
     }
@@ -21,7 +21,36 @@ class Services {
     this.add(name, service, args, "transient")
   }
 
+  resolveDependencies(args) {
+    //dependent array
+    let servicesArr = [];
+    //make a clone of args to avoid interfering with it
+    let newArgs = structuredClone(args)
 
+
+    for (let i = 0; i < newArgs.length; i++) {
+      //check if there is a dependency in the array (starts with $)
+      if (newArgs[i][0] == "$") {
+        //if so push it to the service array
+        servicesArr.push(newArgs[i].substring(1))
+        //pop $dependency from the array (instead the service instance itself will be pushed in the code below)
+        newArgs.pop(i)
+      }
+    }
+
+
+
+    let serviceObj; //holds service Object 
+    for (let index = 0; index < servicesArr.length; index++) {
+      const serviceName = servicesArr[index];
+      // for each service found in teh serviceArr call getService to get the corresponding instance of that service
+      serviceObj = this.getService(serviceName);
+      //push service instance into the args
+      newArgs.push(serviceObj)
+    }
+
+    return newArgs
+  }
   // Retrieve a service by name
   getService(name) {
     const serviceObj = this.services[name];
@@ -32,7 +61,7 @@ class Services {
       throw new Error(`Service ${name} was not registered`);
     }
 
-    if (serviceObj.type == "transient") {
+    if (serviceObj.type == "singleton") {
       //check if service is already instantiated
       if (serviceObj.instantiated) {
         //return the stale service
@@ -42,20 +71,24 @@ class Services {
       //parse service args
       const serviceArgs = serviceObj.serviceArgs
 
+      //resolve any dependency in the arguments of the service
+      let newArgs = this.resolveDependencies(serviceArgs)
+
       //create new instance of the service and store it
-      this.instances[name] = new serviceObj.service(...serviceArgs)
+      this.instances[name] = new serviceObj.service(...newArgs)
       serviceObj.instantiated = true
+
       //return the newly intantiated service
       return this.instances[name];
     }
-    else if(serviceObj.type == "singleton"){
+
+    else if (serviceObj.type == "transient") {
       return this.getNewService(serviceObj)
     }
 
   }
 
   getNewService(serviceObj) {
-
     //check id serice was registered
     if (!serviceObj) {
       //throw error if service was not registered
@@ -64,8 +97,12 @@ class Services {
 
     //parse service args
     const serviceArgs = serviceObj.serviceArgs
+
+    //resolve any dependency in the arguments of the service
+    let newArgs = this.resolveDependencies(serviceArgs)
+
     //return a new instance of the service
-    return new serviceObj.service(...serviceArgs)
+    return new serviceObj.service(...newArgs)
   }
 }
 
